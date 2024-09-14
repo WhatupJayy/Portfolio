ca0b720d264750ff43fce77f0f4e92b36f1d2aed
WhatupJayy <145752556+WhatupJayy@users.noreply.github.com>
2024-09-14 16:36:22 +0530

    Update Header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-cover bg-no-repeat bg-fixed bg-parallax1'>
+    <div className='bg-cover bg-no-repeat bg-fixed bg-parallax1 h-screen'>
         <div className='pt-80 text-white ml-28 font-kohoSemibold text-2xl'>WELCOME TO <br /> JAY'S WORLD.</div>
     </div>
   )
